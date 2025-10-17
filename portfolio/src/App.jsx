dce0f52fa87e85dc64d586ec418c1e990bea6afd
thenameisanzu <ansuvs047@gmail.com>
2025-10-17 10:40:20 +0530

    Initial commit

---
+++ portfolio/src/App.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import Header from "./components/Header";
+
+
+function App() {
+  
+
+  return (
+    <div className="App">
+      <Header/>
+      <main>
+        
+      </main>
+      
+    </div>
+  )
+}
+
+export default App
---
+++ portfolio/src/components/Header.jsx
@@ -0,0 +1,16 @@
+import React from "react"
+
+const Header = () => (
+    <header className="header"> 
+    <div className= "header-content">
+        <h1>Ansu VS</h1>
+        <nav>
+            <a href="#about">About</a>
+        <a href="#projects">Projects</a>
+        <a href="#contact">Contact</a>
+        </nav>
+    </div>
+    </header>
+)
+
+export default Header;
